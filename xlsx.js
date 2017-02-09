67c9f196dce6451759d59331e8292185085ae01f
Francis Crick <fcrick@mz.com>
2017-02-09 12:02:36 -0800

    fixing an issue where _xd_ is getting escaped

--- xlsx.js
+++ xlsx.js
@@ -1383,7 +1383,7 @@ var rencstr = "&<>'\"".split("");
 
 // TODO: CP remap (need to read file version to determine OS)
 var unescapexml = (function() {
-	var encregex = /&[a-z]*;/g, coderegex = /_x([\da-fA-F]+)_/g;
+	var encregex = /&[a-z]*;/g, coderegex = /_x([\da-fA-F]{4})_/g;
 	return function unescapexml(text) {
 		var s = text + '';
 		return s.replace(encregex, function($$) { return encodings[$$]; }).replace(coderegex,function(m,c) {return String.fromCharCode(parseInt(c,16));});
